6eb5b136d8018670474095ee7ed6f95ff62b84a9
steffen <stro889002@aol.com>
2025-12-29 20:54:59 +0100

    Refactor main.rs: remove redundant newlines for cleaner code readability.

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,19 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let (db_host, database, db_user, db_pass) = ("localhost", "wetter", "postgres", "postgres");
-
     let connection_string = format!(
         "host={} user={} password={} dbname={}",
         db_host, db_user, db_pass, database
     );
     let mut data: HashMap<String, f64> = HashMap::new();
-
     let mut client = Client::connect(&connection_string, NoTls)?;
     let query =
         "SELECT \"Time\"::TEXT as time, \"BME280_pressure\" as pressure FROM \"Feinstaubsensor\";";
     let rows = client.query(query, &[])?;
-
     for row in rows {
         let date_opt: Option<String> = row.get("time");
         let pressure_opt: Option<String> = row.get("pressure");
-
         let parsed_pressure = pressure_opt.and_then(|p| p.parse::<f64>().ok());
-
         if let Some((date, pressure)) = date_opt.zip(parsed_pressure) {
             let real_pressure = pressure / 100.0;
             data.insert(date.clone(), real_pressure);
@@ -30,21 +25,16 @@ fn main() -> Result<(), Box<dyn Error>> {
             println!("Überspringe Zeile mit NULL-Werten");
         }
     }
-
     let query = "SELECT \"Zeit\"::TEXT as time, \"Abs. Luftdruck(hpa)\" as pressure FROM \"Wetterstation\";";
-
     let rows = client.query(query, &[])?;
-
     for row in rows {
         let date_opt: Option<String> = row.get("time");
         let pressure_opt: Option<f64> = row.get("pressure");
-
         if let Some((date, pressure)) = date_opt.zip(pressure_opt) {
             if pressure_opt.unwrap() < 100.0 {
                 print!("{} -> ", &date);
                 let date_str = &date[..10];
                 let timeline = make_timeline(&data, date_str);
-
                 for (time, pressure) in timeline {
                     if is_same_hour(&time, &date[11..19]) && is_same_minute(&time, &date[11..19]) {
                         println!("{}: {}", time, pressure);
@@ -56,6 +46,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     Ok(())
 }
+
 fn filter_date(date: &str, date_str: &str) -> bool {
     date.starts_with(date_str)
 }
@@ -65,7 +56,6 @@ fn make_timeline(data: &HashMap<String, f64>, date_str: &str) -> HashMap<String,
     for (date, pressure) in data {
         if filter_date(date, date_str) {
             let time = &date[11..19];
-
             timeline.insert(time.to_string(), pressure.to_string());
         }
     }
